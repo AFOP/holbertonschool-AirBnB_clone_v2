780257d1348a4a3fdebff9edd4d4af4041343a07
AFOP <4592@holbertonschool.com>
2022-09-14 19:59:36 -0500

    Task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
 """Fabric script that generates a .tgz archive
     from the contents of the web_static"""
-from fabric.api import local
-from os.path import isdir
-import time
+
+from fabric.operations import local
+from datetime import datetime
 
 
 def do_pack():
-    """Funtion to generates the file"""
-
-    if isdir('versions') is False:
-        local("mkdir versions")
-    file = "web_static_" + time.strftime("%Y%m%d%H%M%S", time.gmtime())\
-        + ".tgz"
-    result = local("tar -cvzf versions/{} web_static ".format(file))
+    """Function to compress files"""
+    local("mkdir -p versions")
+    result = local("tar -cvzf versions/web_static_{}.tgz web_static"
+                   .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")),
+                   capture=True)
     if result.failed:
         return None
-    return "versions/web_static_" +\
-        time.strftime("%Y%m%d%H%M%S", time.gmtime())\
-        + ".tgz"
+    return result
